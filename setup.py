08435a3a2041e2c5cc5960496284448d01f61b38
Julien Castets <jcastets@ocs.online.net>
2014-11-28 18:17:57 +0100

    Rework setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,36 @@
 from setuptools import setup, find_packages
 
-setup(name='juju-onlinelabs',
-      version="0.0.1",
-      classifiers=[
-          'Intended Audience :: Developers',
-          'Programming Language :: Python',
-          'Operating System :: OS Independent'],
-      author='Edouard Bonlieu',
-      author_email='ebonlieu@ocs.online.net',
-      description="Online Labs integration with juju",
-      url='http://onlinelabs.net',
-      license='BSD',
-      packages=find_packages(),
-      install_requires=["PyYAML", "requests"],
-      tests_require=["nose", "mock"],
-      entry_points={
-          "console_scripts": [
-              'juju-onlinelabs = juju_onlinelabs.cli:main']},
-      )
+
+DEPENDENCIES = [
+    'PyYAML',
+    'requests'
+]
+
+TEST_DEPENDENCIES = [
+    'nose',
+    'mock'
+]
+
+
+setup(
+    name='juju-onlinelabs',
+    version='0.0.1',
+    author='Edouard Bonlieu',
+    author_email='ebonlieu@ocs.online.net',
+    description='Online Labs integration with juju',
+    url='http://onlinelabs.net',
+    license='BSD',
+    packages=find_packages(),
+    install_requires=DEPENDENCIES,
+    tests_require=DEPENDENCIES + TEST_DEPENDENCIES,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent'
+    ],
+    entry_points={
+        'console_scripts': [
+            'juju-onlinelabs = juju_onlinelabs.cli:main'
+        ]
+    }
+)
