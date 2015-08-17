1380df3797e1ae9819eea7b0c8069a7ff323188e
Kevin Deldycke <kdeldycke@ocs.online.net>
2015-08-17 11:36:23 +0200

    Add license headers. Closes #2.

--- juju_scaleway/exceptions.py
+++ juju_scaleway/exceptions.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014-2015 Online SAS and Contributors. All Rights Reserved.
+#                         Edouard Bonlieu <ebonlieu@scaleway.com>
+#                         Julien Castets <jcastets@scaleway.com>
+#                         Manfred Touron <mtouron@scaleway.com>
+#                         Kevin Deldycke <kdeldycke@scaleway.com>
+#
+# Licensed under the BSD 2-Clause License (the "License"); you may not use this
+# file except in compliance with the License. You may obtain a copy of the
+# License at http://opensource.org/licenses/BSD-2-Clause
+
+
 class ConfigError(ValueError):
     """ Environments.yaml configuration error.
     """
